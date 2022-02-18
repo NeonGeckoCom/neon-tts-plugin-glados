ebf96ab430e64b5cedee3f674a8410af280cb0e5
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-02-18 07:27:00 -0800

    Update GH Automation and refactor packaging (#1)
    * Refactor actions for easier readability on GH
    Add actions to publish to PyPI and GHCR
    Add version automation
    Refactor docker build to build from current branch
    
    * rename README.md for consistency with other packages
    
    * Add MANIFEST.in
    
    authored-by: Daniel McKnight <daniel@neon.ai>

--- setup.py
+++ setup.py
@@ -1,25 +1,55 @@
 #!/usr/bin/env python3
+from os import path, getenv
 from setuptools import setup
 
 PLUGIN_ENTRY_POINT = 'neon-tts-plugin-glados = ' \
                      'neon_tts_plugin_glados:GladosTTSPlugin'
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+            if getenv("GITHUB_TOKEN"):
+                if "github.com" in r:
+                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+            requirements[i] = r
+    return requirements
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
 setup(
     name='neon-tts-plugin-glados',
-    version='0.0.1',
+    version=version,
     description='glados tts plugin for OpenVoiceOS',
+    long_description=long_description,
     url='https://github.com/NeonGeckoCom/neon-tts-plugin-glados',
-    author='JarbasAi',
-    author_email='jarbasai@mailfence.com',
+    author='Neongecko',
+    author_email='developers@neon.ai',
     license='Apache-2.0',
     packages=['neon_tts_plugin_glados',
               'neon_tts_plugin_glados.utils'],
-    install_requires=["ovos-plugin-manager>=0.0.4a2",
-                      "ovos-utils~=0.0.15a1",
-                      "phonemizer~=3.0",
-                      "torch~=1.10",
-                      "inflect",
-                      "scipy",
-                      "unidecode"],
+    install_requires=get_requirements("requirements.txt"),
+    extras_require={
+        "docker": get_requirements("docker.txt")
+    },
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
